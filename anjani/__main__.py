340508d279cc9d965ef4f860e72e1e0e89cc1682
Lylia <71930916+dishapatel010@users.noreply.github.com>
2023-03-18 15:10:03 +0530

    Update __main__.py

--- anjani/__main__.py
+++ anjani/__main__.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from flask import Flask
+import threading
 from anjani import main
 
 app = Flask(__name__)
@@ -24,5 +25,7 @@ def index():
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    main.start()
-    app.run(host="0.0.0.0", port=8080)
+    thread1 = threading.Thread(target=main.start)
+    thread2 = threading.Thread(target=app.run, kwargs={"host": "0.0.0.0", "port": 8080})
+    thread1.start()
+    thread2.start()
