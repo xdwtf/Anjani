90be11ff5aab095cbabe7ffdecc69cec0585ebd4
Lylia <71930916+dishapatel010@users.noreply.github.com>
2023-07-06 12:06:26 +0530

    Update misc.py

--- anjani/plugins/misc.py
+++ anjani/plugins/misc.py
@@ -278,62 +278,67 @@ class Misc(plugin.Plugin):
     @listener.priority(95)
     @listener.filters(filters.regex(r"https?://(?:www\.)threads\.net/t/[a-zA-Z0-9_-]+") & filters.group & ~filters.outgoing)
     async def on_message(self, message: Message) -> None:
-        """Threds Media Handler"""
+        """Threads Media Handler"""
         chat = message.chat
         ie = message.reply_to_message or message
         text = message.text
-        # Find all URLs matching the pattern
-        urls = re.findall(r"https?://(?:www\.)threads\.net/t/[a-zA-Z0-9_-]+", text)
-        
-        if not urls:
+
+        try:
+            # Find all URLs matching the pattern
+            urls = re.findall(r"https?://(?:www\.)threads\.net/t/[a-zA-Z0-9_-]+", text)
+
+            if not urls:
+                return None
+
+            for url in urls:
+                post_id_matches = re.findall(r"https?://www\.threads\.net/t/([a-zA-Z0-9_-]+)", url)
+                if not post_id_matches:
+                    continue
+
+                post_id = post_id_matches[0]
+
+                async with httpx.AsyncClient() as http_client:
+                    response = await http_client.get("https://www.threads.net/")
+                    if response.status_code != 200:
+                        return None
+
+                    r = await http_client.get(f"https://www.threads.net/t/{post_id}/embed/")
+                    soup = BeautifulSoup(r.text, "html.parser")
+                    medias = []
+
+                    if div := soup.find("div", {"class": "SingleInnerMediaContainer"}):
+                        if video := div.find("video"):
+                            url = video.find("source").get("src")
+                            medias.append({"p": url, "w": 0, "h": 0})
+                        if image := div.find("img", {"class": "img"}):
+                            url = image.get("src")
+                            medias.append({"p": url, "w": 0, "h": 0})
+
+                    if not medias:
+                        return None
+
+                    files = []
+                    for media in medias:
+                        response = await http_client.get(media["p"])
+                        file = io.BytesIO(response.content)
+                        file.name = f"{media['p'][60:80]}.{filetype.guess_extension(file)}"
+                        files.append({"p": file, "w": media["w"], "h": media["h"]})
+
+                    if not files:
+                        return None
+
+                    for file in files:
+                        filepath = f"{file['p'].name}"
+                        with open(filepath, 'wb') as f:
+                            f.write(file['p'].getbuffer())
+
+                        await self.bot.client.send_document(chat.id, document=filepath, caption=url, reply_to_message_id=ie.id)
+
+                        # Remove the downloaded file
+                        os.remove(filepath)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
             return None
-        
-        for url in urls:
-            post_id_matches = re.findall(r"https?://www\.threads\.net/t/([a-zA-Z0-9_-]+)", url)
-            if not post_id_matches:
-                continue
-
-            post_id = post_id_matches[0]
-
-            async with httpx.AsyncClient() as http_client:
-                response = await http_client.get("https://www.threads.net/")
-                if response.status_code != 200:
-                    return None
-
-                r = await http_client.get(f"https://www.threads.net/t/{post_id}/embed/")
-                soup = BeautifulSoup(r.text, "html.parser")
-                medias = []
-
-                if div := soup.find("div", {"class": "SingleInnerMediaContainer"}):
-                    if video := div.find("video"):
-                        url = video.find("source").get("src")
-                        medias.append({"p": url, "w": 0, "h": 0})
-                    if image := div.find("img", {"class": "img"}):
-                        url = image.get("src")
-                        medias.append({"p": url, "w": 0, "h": 0})
-
-                if not medias:
-                    return None
-                
-                files = []
-                for media in medias:
-                    response = await http_client.get(media["p"])
-                    file = io.BytesIO(response.content)
-                    file.name = f"{media['p'][60:80]}.{filetype.guess_extension(file)}"
-                    files.append({"p": file, "w": media["w"], "h": media["h"]})
-
-                if not files:
-                    return None
-                
-                for file in files:
-                    filepath = f"{file['p'].name}"
-                    with open(filepath, 'wb') as f:
-                        f.write(file['p'].getbuffer())
-
-                    await self.bot.client.send_document(chat.id, document=filepath, caption=url, reply_to_message_id=ie.id)
-             
-                    # Remove the downloaded file
-                    os.remove(filepath)
                 
     @listener.priority(95)
     @listener.filters(filters.regex(platforms_regex) & ~filters.outgoing)
