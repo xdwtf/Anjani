5cd9a5f73d25c7f5ef7cabbad5783222088d9acc
Lylia <71930916+dishapatel010@users.noreply.github.com>
2023-07-09 12:53:00 +0530

    Create lastfm.py

---
+++ anjani/custom_plugins/lastfm.py
@@ -0,0 +1,74 @@
+""" last.fm Plugin """
+import json, requests
+
+from typing import Any, ClassVar, Optional
+
+from aiohttp import ClientConnectorError, ClientSession, ContentTypeError
+from aiopath import AsyncPath
+
+from anjani import command, filters, listener, plugin
+from pyrogram.types import Message, InputMediaPhoto, InputMediaVideo
+from pyrogram.enums.parse_mode import ParseMode
+
+class LastfmPlugin(plugin.Plugin):
+    name = "LAST FM"
+    helpable: ClassVar[bool] = False
+
+    db: util.db.AsyncCollection
+
+    async def on_load(self) -> None:
+        self.db = self.bot.db.get_collection("LASTFM")
+
+    async def get_data(self, key: str, value: Any) -> Optional[MutableMapping[str, Any]]:
+        return await self.db.find_one({key: value})
+    
+    async def set_lastfm_username(self, user_id: int, username: str) -> None:
+        await self.db.update_one({"user_id": user_id}, {"$set": {"lastfm_username": username}}, upsert=True)
+    
+    async def get_lastfm_username(self, user_id: int) -> Optional[str]:
+        data = await self.get_data("user_id", user_id)
+        if data and "lastfm_username" in data:
+            return data["lastfm_username"]
+        return None
+
+    @command.filters(filters.owner_only)
+    async def cmd_setusername(self, ctx: command.Context) -> None:
+        """Set the user's Last.fm username"""
+        if len(ctx.args) < 1:
+            await ctx.respond("Please provide your Last.fm username.")
+            return
+
+        lastfm_username = ctx.args[0]
+        await self.set_lastfm_username(ctx.msg.from_user.id, lastfm_username)
+        await ctx.respond(f"Last.fm username has been set as: {lastfm_username}")
+
+    @command.filters(filters.private)
+    async def cmd_status(self, ctx: command.Context) -> None:
+        """Show the user's Last.fm status"""
+        lastfm_username = await self.get_lastfm_username(ctx.msg.from_user.id)
+
+        if not lastfm_username:
+            await ctx.respond("Last.fm username not found. Please set your Last.fm username using /setusername.")
+            return
+
+        lastfm_api_key = self.bot.config.LASTFM_API_KEY
+
+        url = f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={lastfm_username}&api_key={lastfm_api_key}&format=json&limit=1"
+        response = requests.get(url)
+        data = json.loads(response.text)
+
+        if "error" in data:
+            await ctx.respond("An error occurred while retrieving Last.fm data. Please try again later.")
+            return
+
+        track = data["recenttracks"]["track"][0]
+        artist = track["artist"]["#text"]
+        title = track["name"]
+        is_playing = "@attr" in track and track["@attr"]["nowplaying"] == "true"
+
+        if is_playing:
+            message = f"You are currently listening to:\n\nArtist: {artist}\nTrack: {title}"
+        else:
+            message = f"You recently listened to:\n\nArtist: {artist}\nTrack: {title}"
+
+        await ctx.respond(message)
