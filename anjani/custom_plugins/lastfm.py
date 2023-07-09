fe9af87204fa866f0a784106f150d68036d40579
Lylia <71930916+dishapatel010@users.noreply.github.com>
2023-07-09 16:41:27 +0530

    Update lastfm.py

--- anjani/custom_plugins/lastfm.py
+++ anjani/custom_plugins/lastfm.py
@@ -50,7 +50,7 @@ class LastfmPlugin(plugin.Plugin):
         await self.set_lastfm_username(ctx.msg.from_user.id, lastfm_username)
         await ctx.respond(f"Last.fm username has been set as: {lastfm_username}")
 
-    #@command.filters(filters.private & filters.group)
+    @command.filters((filters.private | filters.group) & filters.create(lambda _, cmd: cmd.text.lower() == "/status" or "status" in cmd.text.lower()))
     async def cmd_status(self, ctx: command.Context) -> None:
         """Show the user's Last.fm status"""
         lastfm_username = await self.get_lastfm_username(ctx.msg.from_user.id)
