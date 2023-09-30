95d7671fd803f56d93e8750b0cf5757c5f07711b
Lylia <71930916+dishapatel010@users.noreply.github.com>
2023-09-30 19:31:22 +0530

    Update ai.py

--- anjani/custom_plugins/ai.py
+++ anjani/custom_plugins/ai.py
@@ -32,10 +32,8 @@ class aiPlugin(plugin.Plugin):
         return None
 
     @command.filters(filters.private)
-    async def cmd_setAI(self, ctx: command.Context) -> None:
+    async def cmd_setai(self, ctx: command.Context) -> None:
         """Set the user's AI info"""
-        print(ctx.args[0])
-        print(ctx.args[1])
         if len(ctx.args) < 2:
             await ctx.respond("Please provide two values as command-line arguments.")
             return
@@ -44,3 +42,37 @@ class aiPlugin(plugin.Plugin):
         api_token = ctx.args[1]
         await self.set_data(ctx.msg.from_user.id, account_id, api_token)
         await ctx.respond(f"AI info has been set as: {account_id} {api_token}")
+    
+    @command.filters(filters.private | filters.group)
+    async def cmd_ai(self, ctx: command.Context) -> None:
+        """WORKER AI API CALL"""
+        if ctx.msg.reply_to_message:
+            if ctx.msg.reply_to_message.text:
+                intext = ctx.msg.reply_to_message.text
+                if len(intext) > 768:
+                    await ctx.respond("Please note that there is a 768 character limit for the replied message.")
+                    return
+        else:
+            await ctx.respond("Reply to Question text")
+            return
+
+        account_id, api_token = await self.get_info(ctx.msg.from_user.id)
+        if not account_id:
+            await ctx.respond("AI info not found. Please set your AI infousing /setai in PM")
+            return
+
+        API_BASE_URL = f"https://api.cloudflare.com/client/v4/accounts/{account_id}/ai/run/"
+        headers = {"Authorization": "Bearer {api_token}"}
+        
+        def ask(model, inputs):
+            input = { "messages": inputs }
+            response = requests.post(f"{API_BASE_URL}{model}", headers=headers, json=input)
+            return response.json()
+        
+        inputs = [
+            { "role": "system", "content": "You are a friendly assistant 256 character output limit" },
+            { "role": "user", "content": intext}
+        ];
+        output = ask("@cf/meta/llama-2-7b-chat-int8", inputs)
+        aimessage = output['result']['response']
+        await ctx.respond(aimessage, disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
